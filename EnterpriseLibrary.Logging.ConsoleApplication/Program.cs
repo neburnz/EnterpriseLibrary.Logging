b8c889fed2f5cc976f82b3cb131d00a0b3b05691
Ruben Guzman <neburnz@hotmail.com>
2014-10-27 12:56:14 -0600

    Fix minimal configuration to use flat file trace listener

--- EnterpriseLibrary.Logging.ConsoleApplication/Program.cs
+++ EnterpriseLibrary.Logging.ConsoleApplication/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Practices.EnterpriseLibrary.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,16 @@ namespace EnterpriseLibrary.Logging.ConsoleApplication
     {
         static void Main(string[] args)
         {
+            //Initializes the Logging Application Block
+            LogWriterFactory logWriterFactory = new LogWriterFactory();
+            LogWriter logWriter = logWriterFactory.Create();
+
+            LogEntry logEntry = new LogEntry();
+            logEntry.Message = "Message logged";
+            logEntry.Categories.Add("General");
+
+            logWriter.Write(logEntry);
+
             Console.WriteLine("Press any key to continue...");
             Console.ReadLine();
         }
